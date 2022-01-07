32df4c447407d67935a88ab7841ff49ee6807b89
aycippo <aylah.cippoletti@icloud.com>
2022-01-07 12:07:24 -0800

    w o a h

--- src/entities.rs
+++ src/entities.rs
@@ -8,11 +8,11 @@ pub struct GameState {
     // has worlds, nations, regions, provinces, popgroups, resources
     pub entity_count: Id,
     pub worlds: Worlds,
-    //pub nations: Nations,
-    //pub regions: Regions,
-    //pub provinces: Provinces,
-    //pub pop_groups: PopGroups,
-    //pub resources: Resources,
+    pub nations: Nations,
+    pub regions: Regions,
+    pub provinces: Provinces,
+    pub pop_groups: PopGroups,
+    pub resources: Resources,
 }
 
 impl GameState {
@@ -20,26 +20,47 @@ impl GameState {
         GameState {
             entity_count: 0,
             worlds: Worlds::new(),
-            //nations: Nations::new(),
-            //regions: Regions::new(),
-            //provinces: Provinces::new(),
-            //pop_groups: PopGroups::new(),
-            //resources: Resources::new(),
+            nations: Nations::new(),
+            regions: Regions::new(),
+            provinces: Provinces::new(),
+            pop_groups: PopGroups::new(),
+            resources: Resources::new(),
         }
     }
 
     pub fn add_world(&mut self, name: Option<Name>, owner: Option<Name>, regions: Vec<RegionId>) {
-        self.worlds.names.insert(self.entity_count, name);
-        self.worlds.owners.insert(self.entity_count, owner);
+        self.worlds.name.insert(self.entity_count, name);
+        self.worlds.owner.insert(self.entity_count, owner);
         self.worlds.regions.insert(self.entity_count, regions);
         self.entity_count += 1;
+    }
+
+    pub fn add_nation(
+        &mut self,
+        name: Option<Name>,
+        worlds: Vec<Option<WorldId>>,
+        regions: Vec<Option<RegionId>>,
+        provinces: Vec<Option<ProvinceId>>,
+        pop_groups: Vec<Option<PopGroupId>>,
+        resources: Vec<Option<ResourceId>>,
+        treasury: u64,
+    ) {
+        self.nations.name.insert(self.entity_count, name);
+        self.nations.worlds.insert(self.entity_count, worlds);
+        self.nations.regions.insert(self.entity_count, regions);
+        self.nations.provinces.insert(self.entity_count, provinces);
+        self.nations
+            .pop_groups
+            .insert(self.entity_count, pop_groups);
+        self.nations.resources.insert(self.entity_count, resources);
+        self.nations.treasury = treasury;
     }
 }
 
 #[derive(Debug, Serialize)]
 pub struct Worlds {
-    names: HashMap<Id, Option<Name>>,
-    owners: HashMap<Id, Option<Name>>,
+    name: HashMap<Id, Option<Name>>,
+    owner: HashMap<Id, Option<Name>>,
     regions: HashMap<Id, Vec<RegionId>>,
     // can change names
 }
@@ -47,49 +68,85 @@ pub struct Worlds {
 impl Worlds {
     pub fn new() -> Worlds {
         Worlds {
-            names: HashMap::new(),
-            owners: HashMap::new(),
+            name: HashMap::new(),
+            owner: HashMap::new(),
             regions: HashMap::new(),
         }
     }
 }
 
 #[derive(Debug, Serialize)]
-struct Nations {
-    // has name, worlds, regions, provinces, money, popgroups, needs/wants, government type, religion, resources, vassals, overlords
-// can aquire new worlds, regions, provinces, can trade, can create or destroy popgroups, can change government, can change religion, can aquire or lose resources, can change names
+pub struct Nations {
+    name: HashMap<Id, Option<Name>>,
+    worlds: HashMap<Id, Vec<Option<WorldId>>>,
+    regions: HashMap<Id, Vec<Option<RegionId>>>,
+    provinces: HashMap<Id, Vec<Option<ProvinceId>>>,
+    // perhaps currency should be its own struct/type for different currency and exchange rates
+    pop_groups: HashMap<Id, Vec<Option<PopGroupId>>>,
+    resources: HashMap<Id, Vec<Option<ResourceId>>>,
+    treasury: u64,
+    // needs/wants, government type, religion, vassals, overlords
+    // can aquire new worlds, regions, provinces, can trade, can create or destroy popgroups, can change government, can change religion, can aquire or lose resources, can change names
 }
 
-impl Nations {}
+impl Nations {
+    pub fn new() -> Nations {
+        Nations {
+            name: HashMap::new(),
+            worlds: HashMap::new(),
+            regions: HashMap::new(),
+            provinces: HashMap::new(),
+            treasury: 0,
+            pop_groups: HashMap::new(),
+            resources: HashMap::new(),
+        }
+    }
+}
 
 #[derive(Debug, Serialize)]
-struct Regions {
+pub struct Regions {
     // has provinces, name, an owner
 // can change names, can change what provinces are in it, can summarize provinces
 }
 
-impl Regions {}
+impl Regions {
+    pub fn new() -> Regions {
+        Regions {}
+    }
+}
 
 #[derive(Debug, Serialize)]
-struct Provinces {
+pub struct Provinces {
     // has popgroups, resources, name, an owner
 // can change names, can change resources, can create and remove popgroups, can change owners
 }
 
-impl Provinces {}
+impl Provinces {
+    pub fn new() -> Provinces {
+        Provinces {}
+    }
+}
 
 #[derive(Debug, Serialize)]
-struct PopGroups {
+pub struct PopGroups {
     // has number of people, has money, has needs/wants, has growth, has worldviews, job type, name, religion, politics
 // can change name, can gain or lose money, can attempt to meet needs/wants, can trade, can change jobs, can change religion, can change politics
 }
 
-impl PopGroups {}
+impl PopGroups {
+    pub fn new() -> PopGroups {
+        PopGroups {}
+    }
+}
 
 #[derive(Debug, Serialize)]
-struct Resources {
+pub struct Resources {
     // has name, value, quantity, extraction rate
 // can run out, can be used, can be created
 }
 
-impl Resources {}
+impl Resources {
+    pub fn new() -> Resources {
+        Resources {}
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,13 @@ use entities::*;
 fn main() {
     let mut game_state: GameState = GameState::new();
 
-    game_state.add_world(Some(Name("Mercury")), Some(Name("USSR")), vec![0, 3, 5]);
-    game_state.add_world(Some(Name("Venus")), Some(Name("USSR")), vec![0, 3, 5]);
-    game_state.add_world(Some(Name("Earth")), Some(Name("USSR")), vec![0, 3, 5]);
-    game_state.add_world(Some(Name("Mars")), Some(Name("USSR")), vec![0, 3, 5]);
-    game_state.add_world(Some(Name("Jupiter")), Some(Name("USSR")), vec![0, 3, 5]);
-    game_state.add_world(Some(Name("Saturn")), Some(Name("USSR")), vec![0, 3, 5]);
-    game_state.add_world(Some(Name("Uranus")), Some(Name("USSR")), vec![0, 3, 5]);
-    game_state.add_world(Some(Name("Neptune")), Some(Name("USSR")), vec![0, 3, 5]);
+    game_state.add_world(Some(Name("Earth")), None, Vec::new());
 
     let data = serde_json::to_string(&game_state).unwrap();
-    println!("{}", data);
+    println!("{:#?}", game_state);
 }
+
+//by what metric are you rating trades and how to weight them
+// data strucutes, every nation and how they connect, distance/weight/value between them
+// make nations produce then consume
+// fill a map with planets
