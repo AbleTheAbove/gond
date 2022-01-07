f2bb511edd7a91cfb4f65bf3db7583f9ea963d70
Able <abl3theabove@gmail.com>
2022-01-07 16:52:05 -0600

    added various things

--- src/entities.rs
+++ src/entities.rs
@@ -37,48 +37,59 @@ impl GameState {
 
 #[derive(Debug, Serialize)]
 struct Worlds {
+    name: String,
     // has regions, nations, name, owner
-// can change names
+    // can change names
 }
 
 impl Worlds {}
 
 #[derive(Debug, Serialize)]
 struct Nations {
+    name: String,
+    regions: Vec<Regions>,
+    provinces: Vec<Provinces>,
     // has name, worlds, regions, provinces, money, popgroups, needs/wants, government type, religion, resources, vassals, overlords
-// can aquire new worlds, regions, provinces, can trade, can create or destroy popgroups, can change government, can change religion, can aquire or lose resources, can change names
+    // can aquire new worlds, regions, provinces, can trade, can create or destroy popgroups, can change government, can change religion, can aquire or lose resources, can change names
 }
 
 impl Nations {}
 
 #[derive(Debug, Serialize)]
 struct Regions {
+    name: String,
     // has provinces, name, an owner
-// can change names, can change what provinces are in it, can summarize provinces
+    // can change names, can change what provinces are in it, can summarize provinces
 }
 
 impl Regions {}
 
 #[derive(Debug, Serialize)]
 struct Provinces {
+    name: String,
     // has popgroups, resources, name, an owner
-// can change names, can change resources, can create and remove popgroups, can change owners
+    // can change names, can change resources, can create and remove popgroups, can change owners
 }
 
 impl Provinces {}
 
 #[derive(Debug, Serialize)]
 struct PopGroups {
+    name: String,
+    population_count: u64,
     // has number of people, has money, has needs/wants, has growth, has worldviews, job type, name, religion, politics
-// can change name, can gain or lose money, can attempt to meet needs/wants, can trade, can change jobs, can change religion, can change politics
+    // can change name, can gain or lose money, can attempt to meet needs/wants, can trade, can change jobs, can change religion, can change politics
 }
 
 impl PopGroups {}
 
 #[derive(Debug, Serialize)]
 struct Resources {
+    name: String,
+    value: u64,
+    quantity: u64,
     // has name, value, quantity, extraction rate
-// can run out, can be used, can be created
+    // can run out, can be used, can be created
 }
 
 impl Resources {}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 mod components;
 mod entities;
 
-use components::*;
 use entities::*;
 
 fn main() {
-    let mut game_state: GameState = GameState::new();
+    let game_state: GameState = GameState::new();
     let data = serde_json::to_string(&game_state).unwrap();
     println!("{}", data);
 }
